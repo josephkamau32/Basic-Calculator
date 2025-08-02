683b47441d1c6e74e01c8694c51fae26768ab6dd
Joseph Kamau <iamjosephkamau@gmail.com>
2025-08-02 16:06:20 +0300

    Added new file

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#Basic Calculator Program
+
+#Ask user to input first number
+num1 = float(input("Enter the first number: "))
+
+#Ask user to input second number
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+
+difference_result = num1 - num2
+
+product_result = num1 * num2
+
+quotient_result = num1 / num2
+
+#Print all your results
+
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")  # ➕
+print(f"Difference: {difference_result}")  # ➖
+print(f"Product: {product_result}")  # ✖️
+print(f"Quotient: {quotient_result}")  # ➗
+
